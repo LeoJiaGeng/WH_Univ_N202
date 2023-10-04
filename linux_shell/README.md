c80c488f22ee52fbf309f98a8a76bebef36ef98b
xiongjiageng <jgxiong@whu.edu.cn>
2023-10-04 14:33:36 +0800

    时间：2023.10.4 内容：第一次基线脚本，提交linux中自动上传任务的脚本

---
+++ linux_shell/README.md
@@ -0,0 +1,4 @@
+# linux_shell
+## queue
+
+To creat a queue to submit task one by one within the pre-set max cores number 
---
+++ linux_shell/queue.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "input the maximum cores";
+max_core=$1
+echo "the maximum cores is $max_core";
+
+for file in *.gjf; do
+    echo ${file} # print the file name
+
+    # endless loop infinite
+    while true
+    do
+        # search the number of cores left
+        vmstat > current_core.out
+        sleep 1s
+        vmstat > current_core.out
+        sleep 1s
+        vmstat > current_core.out
+        echo refresh the information of cores once
+
+        dataline=$(cat current_core.out)
+        cur_cores=${dataline:159:160}
+        cur_core=${cur_cores:2:4}
+
+        # set every file cores and calculate the number of cores later
+        file_core=16
+        add_core=`expr $file_core + $cur_core`
+
+        # compare the number of cores to the number of cores
+        if ((($cur_core < $max_core) && ($add_core < $max_core)))
+        then
+            time g16 < ${file} > ${file//gjf/log} &
+            echo ${file} has been submitted
+            break
+        else
+            sleep 30s
+            echo ${file} is waiting 
+        fi
+    done
+done
+
+
