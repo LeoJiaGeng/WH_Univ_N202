d7cf65849e05cb666eb89272abda06d192013f06
xiongjiageng <jgxiong@whu.edu.cn>
2024-01-04 10:32:27 +0800

    时间：2024.1.4 内容：提交修改文件名

--- window_python/rename.py
+++ window_python/rename.py
@@ -1,11 +1,21 @@
 import os 
 
-input_dir = r"D:\Document\Python_Files\test"
-rename = "TS2"
-
+# Read a temporary directory
+input_dir = r"C:\Users\DELL\Desktop\transfer"
 file_list = os.listdir(input_dir)
 
-for filename in file_list:
+# input the list of new filenames
+rename1 = "TS3"
+rename2 = "TS2"
+rename_list = [rename1] * 3 + [rename2] * 3
+
+for filename, rename in zip(file_list, rename_list):
+    # get the file suffix
     portion = os.path.splitext(filename)
+    # combine the old file suffix with the new name
     new_name = rename + portion[1]
+    print(filename,new_name)
+    # replace file name, 需要修改绝对路径
     os.replace(filename, new_name)
+
+print("Success!")
